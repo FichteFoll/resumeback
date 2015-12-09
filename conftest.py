9015f3049ca2277b8d8415c1768d7491fe985e36
FichteFoll <fichtefoll2@googlemail.com>
2015-12-09 03:16:29 +0100

    Python 2 needs indices for str.format

--- conftest.py
+++ conftest.py
@@ -12,9 +12,9 @@ major = sys.version_info[0]
 other_major = {2: 3, 3: 2}[major]
 
 for name in os.listdir(tests_dir):
-    rel_path = "{}/{}".format(tests_dir, name)
+    rel_path = "{0}/{1}".format(tests_dir, name)
     if (
         os.path.isfile(rel_path)
-        and name.endswith("_py{}.py".format(other_major))
+        and name.endswith("_py{0}.py".format(other_major))
     ):
         collect_ignore.append(rel_path)
