f40766aaae8787e358811e08cc8a72c9965c2d5b
FichteFoll <fichtefoll2@googlemail.com>
2015-12-11 17:20:20 +0100

    Read docs version from source

--- setup.py
+++ setup.py
@@ -1,25 +1,7 @@
-import io
-import re
-import os
-
 from setuptools import setup
 
+from _util import read, find_version
 
-def read(*names, **kwargs):
-    with io.open(
-        os.path.join(os.path.dirname(__file__), *names),
-        encoding=kwargs.get("encoding", "utf8")
-    ) as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = (['\"])(.*?)\1",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(2)
-    raise RuntimeError("Unable to find version string.")
 
 setup(
     name='resumeback',
